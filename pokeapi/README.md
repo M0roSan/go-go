720ad97295fba61987397271ae9ed8b40eca6440
MasahiroYoshida <masahiro.yoshida.se@gmail.com>
2019-08-22 20:07:03 -0500

    poke api

---
+++ pokeapi/README.md
@@ -0,0 +1,3 @@
+REST API hitting endpoint to get all the pokemon from the original series(kanto)
+
+Reference: https://tutorialedge.net/golang/consuming-restful-api-with-go/
---
+++ pokeapi/cmd/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+    "encoding/json"
+	
+)
+
+// response struct to map the entire response
+type Response struct {
+	Name    string    `json:"name"`
+	Pokemon []Pokemon `json:"pokemon_entries"`
+}
+
+// a pokemon struc to map every pokemon to
+type Pokemon struct {
+	EntryNo int            `json:"entry_number"`
+	Species PokemonSpecies `json:"pokemon_species"`
+}
+
+// a struct to map our pokemon species which includes its name
+type PokemonSpecies struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	resp, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
+	
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	respData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject Response
+    json.Unmarshal(respData, &responseObject)
+
+    fmt.Println(responseObject.Name)
+    fmt.Println(len(responseObject.Pokemon))
+
+    for i := 0; i < len(responseObject.Pokemon); i++ {
+        fmt.Println(responseObject.Pokemon[i].Species.Name)
+    }
+
+}
