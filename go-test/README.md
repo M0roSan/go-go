543253a4b981c4c004bca4186279e0d3d0a2f796
MasahiroYoshida <masahiro.yoshida.se@gmail.com>
2019-08-25 20:35:33 -0500

    add more benchmark

--- go-test/README.md
+++ go-test/README.md
@@ -7,12 +7,23 @@ go test
 `
 go test -v
 `
+test coverage
 `
 go test -cover
 `
+generating coverage report
 `
 go test -coverprofile=coverage.out
 `
+show the detail report
 `
 go tool cover -html=coverage.out
+`
+performance test use Benchmark prefix and testing.B
+`
+go test -bench=.
+`
+Regex pattern
+`
+go test -run=Calc -bench=.
 `
--- go-test/main_test.go
+++ go-test/main_test.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 
 func TestCalculate(t *testing.T) {
-	if Calculate(2) != 4 {
-		t.Error("Expected 2 + 2 to equal 4")
-
+	expected := 4
+	result := Calculate(2)
+	if expected != result {
+		t.Error("Failed")
 	}
 }
+
+func benchmarkCalculate(input int, b *testing.B){
+	for i := 0; i < b.N; i++ {
+		Calculate(input)
+	}
+}
+
+func BenchmarkCalculate100(b *testing.B) { benchmarkCalculate(100, b)}
+func BenchmarkCalculateNegative100(b *testing.B) { benchmarkCalculate(-100, b)}
+func BenchmarkCalculateNegative1(b *testing.B) { benchmarkCalculate(-1, b)}
 
 
 func TestTableCalculate(t *testing.T) {
@@ -30,4 +42,9 @@ func TestTableCalculate(t *testing.T) {
 			t.Error("Test Failed: {} inputted, {} expected, received: {}", test.input, test.expected, output)
 		}
 	}
+}
+
+func TestOther(t *testing.T){
+	fmt.Println("Testing something else")
+	fmt.Println("This shouldn't run with -run=calc")
 }
